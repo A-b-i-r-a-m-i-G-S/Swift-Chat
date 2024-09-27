50258dc464ef8dd3685d60c1d9d6d08451a53c1b
Abi <abiramigs21@gmail.com>
2024-09-27 21:18:46 +0530

    Created the profile edit page

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+  const [image, setImage] = useState(false);
+
   return (
-    <div>ProfileUpdate</div>
+    <div className="profile">
+      <div className="profile-container">
+        <form >
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id='avatar'accept='.png, .jpg, .jpeg' hidden onChange={(e) => setImage(e.target.files[0])}/>
+            <img src={image === false ? assets.avatar_icon : URL.createObjectURL(image)} alt="" />
+            upload profile picture
+          </label>
+
+          <input type="text" placeholder='Name' required/>
+          <textarea placeholder='Profile Bio'></textarea>
+          <button type='submit'>Save</button>
+        </form>
+
+        <img className='profile-pic' src={image === false ? assets.logo_icon : URL.createObjectURL(image)} alt="" />
+      </div>
+    </div>
   )
 }
 
